38adab9cd480198207b0f3c7ea059b42f6243b91
Conrad <conrad@devlabtech.com>
2013-11-02 00:06:15 -0700

    footer separation

---
+++ footer.php
@@ -0,0 +1,35 @@
+    <footer>
+      <div class="container">
+          <div class="col-md-5 col-md-offset-4">
+            <h2 class="pull-right">Subscribe to Our Mailing List for Updates:</h2>
+          </div><!-- /end .col-md-8 -->
+            <form>
+              <div class="col-md-3">
+                <input type="email" placeholder="Email Address" class="form-control pull-left">
+                <button type="submit" class="btn btn-default pull-right">Submit</button>
+              </div>
+            </form> 
+          </div><!-- /end .col-md-4 -->
+        </div><!-- /end .row -->
+      </div><!-- /end .container -->
+    </footer>
+
+    <script>
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-39574765-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
